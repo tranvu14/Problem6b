0815621f18a3b226e7a509fb7e72484eda278bd8
tranvu14 <43311181+tranvu14@users.noreply.github.com>
2018-09-22 22:40:20 +0700

    Add files via upload

---
+++ Caculator/app/src/main/java/com/example/administrator/caculator/NumberOperator.java
@@ -0,0 +1,28 @@
+package com.example.administrator.caculator;
+
+public class NumberOperator {
+
+    private int number;
+    private String operator;
+
+    public NumberOperator(int number, String operator) {
+        this.number = number;
+        this.operator = operator;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+}
